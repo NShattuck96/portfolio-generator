e5fcd64b121d4c0baae3da4de6029639a7786a6c
Nathan Shattuck <Nshattuck96@gmail.com>
2022-05-19 18:28:32 -0400

    update

--- app.js
+++ app.js
@@ -1,14 +1,10 @@
 const fs = require('fs');
 const generatePage = require('./src/page-template');
 
+const pageHTML = generatePage(name, github);
 
-const profileDataArgs = process.argv.slice(2);
-
-const [name, github] = profileDataArgs;
-
-
-fs.writeFile('./index.html', generatePage(name, github), err => {
-  if (err) throw new Error(err);
+fs.writeFile('./index.html', pageHTML, err => {
+  if (err) throw err;
 
   console.log('Portfolio complete! Check out index.html to see the output!');
 });
